89f56ee257dea73096a4cb689fba3d2d09fdfad3
akshitr25 <akshitrathod1025@gmail.com>
2024-07-30 19:40:58 +0530

    Get and Delete APIs to check

--- src/main/java/com/ets/expense_tracking_system/controller/ExpenseController.java
+++ src/main/java/com/ets/expense_tracking_system/controller/ExpenseController.java
@@ -3,7 +3,10 @@ package com.ets.expense_tracking_system.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ets.expense_tracking_system.entity.Expense;
@@ -16,5 +19,13 @@ public class ExpenseController {
 	@GetMapping("/expenses")
 	public List<Expense> getAllExpenses(){
 		return expsenseService.getAllExpenses();
+	}
+	@GetMapping("/expenses/{id}")
+	public String getExpenseById(@PathVariable("id") Long id){
+		return "The expense ID is: "+id;
+	}
+	@DeleteMapping("/expenses")
+	public String deleteExpenseById(@RequestParam("id") Long id){
+		return "The expense ID is: "+id;
 	}
 }
